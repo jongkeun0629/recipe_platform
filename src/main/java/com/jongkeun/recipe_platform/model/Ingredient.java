9f4b6eaa5c4a57e9ab3ab0446e732f0af86d7af1
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-17 09:53:44 +0900

    first commit, Model Create

---
+++ src/main/java/com/jongkeun/recipe_platform/model/Ingredient.java
@@ -0,0 +1,22 @@
+package com.jongkeun.recipe_platform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ingredients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String name;
+}
---
+++ src/main/java/com/jongkeun/recipe_platform/model/Recipe.java
@@ -0,0 +1,25 @@
+package com.jongkeun.recipe_platform.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "recipes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
