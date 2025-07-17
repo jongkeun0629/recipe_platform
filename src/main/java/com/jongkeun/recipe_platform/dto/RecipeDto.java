c42225ab6d86af30f4d61d6d179529a5e15dec7d
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-17 11:20:59 +0900

    Recipe Dto

---
+++ src/main/java/com/jongkeun/recipe_platform/dto/RecipeDto.java
@@ -0,0 +1,14 @@
+package com.jongkeun.recipe_platform.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RecipeDto {
+    @NotBlank
+    private String title;
+
+    private String description;
+}
