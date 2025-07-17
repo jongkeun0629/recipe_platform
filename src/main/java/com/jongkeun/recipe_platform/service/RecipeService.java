e5f0e4b2dc3c60a1f6092981a9b427cc9b85c149
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-07-17 13:37:10 +0900

    Add Ingredient

--- src/main/java/com/jongkeun/recipe_platform/service/RecipeService.java
+++ src/main/java/com/jongkeun/recipe_platform/service/RecipeService.java
@@ -1,14 +1,20 @@
 package com.jongkeun.recipe_platform.service;
 
+import com.jongkeun.recipe_platform.dto.AddIngredientDto;
 import com.jongkeun.recipe_platform.dto.RecipeDto;
 import com.jongkeun.recipe_platform.dto.RecipeResponseDto;
+import com.jongkeun.recipe_platform.model.Ingredient;
 import com.jongkeun.recipe_platform.model.Recipe;
+import com.jongkeun.recipe_platform.model.RecipeIngredient;
+import com.jongkeun.recipe_platform.model.RecipeIngredientId;
 import com.jongkeun.recipe_platform.repository.IngredientRepository;
 import com.jongkeun.recipe_platform.repository.RecipeIngredientRepository;
 import com.jongkeun.recipe_platform.repository.RecipeRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +32,28 @@ public class RecipeService {
         Recipe saved = recipeRepository.save(recipe);
 
         return new RecipeResponseDto(saved.getId(), saved.getTitle(), saved.getDescription());
+    }
+
+    public void addIngredient(Long recipeId, AddIngredientDto dto){
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> new NoSuchElementException("레시피를 찾을 수 없습니다."));
+
+        Ingredient ingredient = ingredientRepository.findById(dto.getIngredientId())
+                .orElseThrow(() -> new NoSuchElementException("재료를 찾을 수 없습니다."));
+
+        RecipeIngredientId id = new RecipeIngredientId(recipeId, ingredient.getId());
+        if (recipeIngredientRepository.existsById(id)){
+            throw new IllegalStateException("재료가 이미 있습니다.");
+        }
+
+        RecipeIngredient recipeIngredient = new RecipeIngredient();
+        recipeIngredient.setId(id);
+        recipeIngredient.setRecipe(recipe);
+        recipeIngredient.setIngredient(ingredient);
+        recipeIngredient.setQuantity(dto.getQuantity());
+
+        recipe.getRecipeIngredients().add(recipeIngredient);
+
+        recipeRepository.save(recipe);
     }
 }
